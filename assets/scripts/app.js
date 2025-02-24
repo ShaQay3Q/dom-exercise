08b4a2c2754dd73d01ae193c7c962b5eebe41455
ShaQay3Q <shaghay3gh@gmail.com>
2025-02-24 13:13:51 +0100

    addEventListiner to cancelBtn and backDropElement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,13 +3,21 @@ const startAddMovieBtn = document.querySelector("header button");
 const backDropElement = document.getElementById("backdrop");
 const addMovieModalElement = document.getElementById("add-modal");
 
+const cancelModalActionsBtnElement = document.querySelector(
+	".modal__actions .btn--passive"
+);
+
+console.log(cancelModalActionsBtnElement);
+
 const toggleBackDrop = () => {
 	backDropElement.classList.toggle("visible");
 };
 
 const toggleMovieModal = () => {
 	addMovieModalElement.classList.toggle("visible");
-    toggleBackDrop();
+	toggleBackDrop();
 };
 
 startAddMovieBtn.addEventListener("click", toggleMovieModal);
+cancelModalActionsBtnElement.addEventListener("click", toggleMovieModal);
+backDropElement.addEventListener("click", toggleMovieModal);
